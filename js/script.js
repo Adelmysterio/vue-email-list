5ce9ffd279eb71c34b38f1332ef40ac7c284a466
Adelmysterio <storliniadelmo@gmail.com>
2024-06-06 09:22:10 +0200

    implementato esercizio

--- js/script.js
+++ js/script.js
@@ -3,16 +3,24 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
+            emails: [],
         }
     },
 
     methods: {
-
+        getEmails: function () {
+            for (let index = 0; index < 10; index++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        const result = response.data.response
+                        this.emails.push(result)
+                    })
+            }
+        }
     },
 
     created() {
-       
+        this.getEmails()
     }
 
 }).mount('#app')
